5d13fb14cdc974072d24adc7d97bc33d214d6515
lailaporter55 <lmp3003@rit.edu>
2025-11-14 13:13:04 -0500

    domos

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -22,20 +22,26 @@ const makeDomo = async (req, res) => {
         return res.status(500).json({error: 'An error occured making domo!'}); 
     }
 }
+const makerPage = (req, res) => {
+    return res.reder('app');
+};
 
-const makerPage = async (req, res) => {
+//getDomos function to retrieve all of the domos belonging to the user
+//allow us to get JSON responses, allow client app to update dynamically using react
+const getDomos = async (req, res) => {
     try{
-        const query = {owner: req.session.account._id}; 
-        const docs = await Domo.find(query).select('name age').lean().exec(); 
+        const query = {owner: req.session.account._id};
+        const docs = await Domo.find(query).select('name age').lean().exec();
 
-        return res.render('app', {domos: docs}); 
-    }catch (err){
-        console.log(err); 
-        return res.status(500).json({error: 'Error retrieving domos!'}); 
+        return res.json({domos: docs}); 
+    } catch(err){
+        console.log(err);
+        return res.status(500).json({error: 'Error retrieving domos!'});
     }
-}
+}; 
 
 module.exports = {
     makerPage, 
     makeDomo, 
+    getDomos,
 }
