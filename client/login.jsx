84160017312996b4283fcc99cf605d62a32810e0
lailaporter55 <lmp3003@rit.edu>
2025-11-14 12:19:53 -0500

    helper

---
+++ client/login.jsx
@@ -0,0 +1,2 @@
+//Login.js will handle everything before the user is logged in, like the login
+//and signup screens
---
+++ client/maker.jsx
@@ -0,0 +1,2 @@
+//will contain all of our code for the application that shows up once the user
+//has logged in
