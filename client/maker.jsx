367e2dafc5be9bdedf9ec82f537607573d10a605
lailaporter55 <lmp3003@rit.edu>
2025-11-14 13:01:47 -0500

    maker

--- client/maker.jsx
+++ client/maker.jsx
@@ -1,2 +1,28 @@
 //will contain all of our code for the application that shows up once the user
 //has logged in
+
+const { makeDomo } = require("../server/controllers/Domo");
+
+const makerPage = (req, res) => {
+    return res.reder('app');
+};
+
+//getDomos function to retrieve all of the domos belonging to the user
+//allow us to get JSON responses, allow client app to update dynamically using react
+const getDomos = async (req, res) => {
+    try{
+        const query = {owner: req.session.account._id};
+        const docs = await Domo.find(query).select('name age').lean().exec();
+
+        return res.json({domos: docs}); 
+    } catch(err){
+        console.log(err);
+        return res.status(500).json({error: 'Error retrieving domos!'});
+    }
+}; 
+
+module.exports = {
+    makerPage, 
+    makeDomo, 
+    getDomos
+}
